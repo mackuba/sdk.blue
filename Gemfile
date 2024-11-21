56c0f9642017bf290cc0f89678934671b6303e4b
Kuba Suder <jakub.suder@gmail.com>
2024-11-21 04:51:48 +0200

    added deploy config

--- Gemfile
+++ Gemfile
@@ -4,3 +4,11 @@ gem "jekyll", "~> 4.3"
 
 group :jekyll_plugins do
 end
+
+group :development do
+  gem 'capistrano', '~> 2.0'
+
+  # for net-ssh
+  gem 'ed25519', '>= 1.2', '< 2.0'
+  gem 'bcrypt_pbkdf', '>= 1.0', '< 2.0'
+end
---
+++ config/deploy.rb
@@ -0,0 +1,51 @@
+# TODO: migrate to capistrano3 bundler integration
+require 'bundler/capistrano'
+set :bundle_dir, ''
+set :bundle_flags, '--quiet'
+set :bundle_without, []
+
+set :application, "sdk.blue"
+set :repository, "https://github.com/mackuba/sdk.blue.git"
+set :scm, :git
+set :keep_releases, 10
+set :use_sudo, false
+set :deploy_to, "/var/www/sdk.blue"
+set :deploy_via, :remote_cache
+set :public_children, []
+
+server "blue.mackuba.eu", :app, :web, :db, :primary => true
+
+before 'bundle:install', 'deploy:set_bundler_options'
+after 'deploy:update_code', 'deploy:link_shared'
+
+after 'deploy', 'deploy:build', 'deploy:cleanup'
+after 'deploy:cold', 'deploy:fetch_metadata', 'deploy:build', 'deploy:cleanup'
+
+namespace :deploy do
+  task :restart, :roles => :web do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  task :set_bundler_options do
+    run "cd #{release_path} && bundle config set --local deployment 'true'"
+    run "cd #{release_path} && bundle config set --local path '#{shared_path}/bundle'"
+    run "cd #{release_path} && bundle config set --local without 'development test'"
+  end
+
+  task :link_shared do
+    run "mkdir -p #{release_path}/config"
+    run "ln -s #{shared_path}/auth.yml #{release_path}/config/auth.yml"
+    run "ln -s #{shared_path}/github_info.yml #{release_path}/_data/github_info.yml"
+  end
+
+  task :build do
+    run "cd #{release_path} && RACK_ENV=production bundle exec jekyll build"
+  end
+
+  task :migrate do
+  end
+
+  task :fetch_metadata do
+    run "cd #{release_path} && RACK_ENV=production bundle exec rake update_metadata"
+  end
+end
