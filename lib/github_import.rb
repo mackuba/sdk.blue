1d9b185ef5f2f1335588f18cb229853b8e318c42
Kuba Suder <jakub.suder@gmail.com>
2024-11-23 18:14:07 +0200

    even more splitting

--- lib/github_import.rb
+++ lib/github_import.rb
@@ -16,6 +16,26 @@ class GithubImport
     url =~ %r{^https://github\.com/([\w\-\.]+)/([\w\-\.]+)}
     user, repo = $1, $2
 
+    data = get_repo_info(user, repo)
+
+    if release = get_latest_release(user, repo)
+      data['last_release'] = release
+    end
+
+    if tag_info = get_latest_tag(user, repo)
+      data['last_tag'] = tag_info
+    end
+
+    data['last_commit'] = get_latest_commit(user, repo)
+
+    if user_info = get_user_info(user)
+      data['user_name'] = user_info['name']
+    end
+
+    data
+  end
+
+  def get_repo_info(user, repo)
     response = get_response("https://api.github.com/repos/#{user}/#{repo}")
     raise FetchError.new(response) unless response.code.to_i == 200
 
@@ -33,12 +53,16 @@ class GithubImport
       data['license'] = json['license']['spdx_id']
     end
 
+    data
+  end
+
+  def get_latest_release(user, repo)
     response = get_response("https://api.github.com/repos/#{user}/#{repo}/releases/latest")
 
     if response.code.to_i == 200
       json = JSON.parse(response.body)
 
-      data['last_release'] = {
+      {
         'tag_name'     => json['tag_name'],
         'name'         => json['name'],
         'draft'        => json['draft'],
@@ -46,10 +70,14 @@ class GithubImport
         'created_at'   => json['created_at'],
         'published_at' => json['published_at']
       }
-    elsif response.code.to_i != 404
+    elsif response.code.to_i == 404
+      nil
+    else
       raise FetchError.new(response)
     end
+  end
 
+  def get_latest_tag(user, repo)
     response = get_response("https://api.github.com/repos/#{user}/#{repo}/tags")
     raise FetchError.new(response) unless response.code.to_i == 200
 
@@ -61,37 +89,38 @@ class GithubImport
       raise FetchError.new(response) unless response.code.to_i == 200
 
       json = JSON.parse(response.body)
-      data['last_tag'] = {
+      {
         'name'           => tag_name,
         'author_date'    => json['commit']['author']['date'],
         'committer_date' => json['commit']['committer']['date']
       }
+    else
+      nil
     end
+  end
 
+  def get_latest_commit(user, repo)
     response = get_response("https://api.github.com/repos/#{user}/#{repo}/commits")
     raise FetchError.new(response) unless response.code.to_i == 200
 
     json = JSON.parse(response.body)
 
     if commit = json.first
-      data['last_commit'] = {
+      {
         'author_date'    => commit['commit']['author']['date'],
         'committer_date' => commit['commit']['committer']['date']
       }
     else
       raise FetchError.new(response)
     end
+  end
 
-    if @user_cache[user].nil?
+  def get_user_info(user)
+    @user_cache[user] ||= begin
       response = get_response("https://api.github.com/users/#{user}")
       raise FetchError.new(response) unless response.code.to_i == 200
 
-      json = JSON.parse(response.body)
-      @user_cache[user] = json
+      JSON.parse(response.body)
     end
-
-    data['user_name'] = @user_cache[user] && @user_cache[user]['name']
-
-    data
   end
 end
